14a12bef115e34eff553096c175be3368be51247
timbaron <timothy.akiode@student.aul.edu.ng>
2023-03-27 10:16:11 +0100

    Project initialization

---
+++ app/index.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import { SafeAreaView, Text } from 'react-native'
+
+const Home = () => {
+  return (
+    <SafeAreaView>
+        <Text>Welcome to my first page</Text>
+    </SafeAreaView>
+  )
+}
+
+export default Home
